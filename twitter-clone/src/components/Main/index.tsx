5bdcd3be39056a4c99c3302b7a9e00e714eb70bd
Guilherme <guiduck02@gmail.com>
2021-03-22 20:36:14 -0300

    feita arquitetura do Main e Header

---
+++ twitter-clone/src/components/Main/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong> Guilherme Figueiredo </strong>
+          <span>612 Tweets</span>  
+        </ProfileInfo>  
+      </Header>
+
+      {/* <ProfilePage />
+      <BottomMenu>
+        <HomeIcon/>
+        <SearchIcon/>
+        <BellIcon/>
+        <EmailIcon/>
+      </BottomMenu> */}
+    </Container>
+  );
+}
+
+export default Main;
