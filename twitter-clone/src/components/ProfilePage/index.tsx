a65de2eafb912e8e019524cfd63f81f0543627f0
Guilherme <guiduck02@gmail.com>
2021-03-24 22:56:00 -0300

    profiloepage quase pronta

---
+++ twitter-clone/src/components/ProfilePage/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage 
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        {/* <EditButton outlined>Editar Perfil</EditButton> */}
+          <h1>Guilherme Figueiredo</h1>
+          <h2>@gui0f</h2>
+
+          <p>
+            Developer at <a href='https://github.com/guiduck'></a>
+          </p>
+
+          <ul>
+            <li>
+              <LocationIcon />
+              Brasilia, DF
+            </li>
+            <li>
+              <CakeIcon />
+              Nascido(a) em 14 de maio de 1995 
+            </li>
+          </ul>
+          <Followage>
+            <span>
+              Seguindo <strong>124</strong>
+            </span>
+            <span>
+              <strong>32 </strong>Seguidores
+            </span>
+          </Followage>
+      </ProfileData>
+    </Container>
+    
+  );
+}
+
+export default ProfilePage;
