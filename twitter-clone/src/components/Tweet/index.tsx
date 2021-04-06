ab9fb65a6d3738c470a3f56884f52dabed57c712
Guilherme <guiduck02@gmail.com>
2021-04-05 22:11:14 -0300

    messing with tweet component

--- twitter-clone/src/components/Tweet/index.tsx
+++ twitter-clone/src/components/Tweet/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+  Container, 
+  Retweeted, 
+  Avatar, 
+  RocketseatIcon, 
+  Body, 
+  Content, 
+  Header, 
+  Description, 
+  ImageContent, 
+  Icons, 
+  Status, 
+  CommentIcon, 
+  RetweetIcon, 
+  LikeIcon 
+} from './styles';
 //do pretty stuff right here
 const Tweet: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Retweeted>
+        <RocketseatIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Rocketseat</strong>
+            <span>@rocketseat</span>
+            {/* <Dot /> */}
+            <time>27 de jun</time>
+          </Header>
+          
+          <Description>Foguete não tem ré</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              18
+            </Status>
+            <Status>
+              <RetweetIcon />
+              18
+            </Status>
+            <Status>
+              <LikeIcon />
+              999
+            </Status>
+          </Icons>
+
+        </Content>
+      </Body>
+    </Container>
+  );
 }
 
 export default Tweet;
