e3a33442f4c5fe214b04790a9e401f13b7ac226c
Guilherme <guiduck02@gmail.com>
2021-05-23 21:08:47 -0300

    spacing menu items

--- twitter-clone/src/components/MenuBar/styles.ts
+++ twitter-clone/src/components/MenuBar/styles.ts
@@ -51,7 +51,52 @@ export const Logo = styled(Rocketseat)`
   margin-bottom: 20px;
 `;
 
-export const MenuButton = styled.div``;
+export const MenuButton = styled.button`
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+
+  > span {
+    display: none;
+  }
+
+  @media (min-width: 1280px) {
+    >span {
+      display: inline;
+      margin-left: 19px;
+
+      font-weight: bold;
+      font-size: 19px;
+    }
+  }
+
+  padding: 8.25px 0;
+  outline: 0;
+
+  & + button {
+    margin-top: 16.5px;
+  }
+
+  & + button:last-child {
+    margin-top: 33px;
+
+    width: 40px;
+    height: 40px;
+
+    >span {
+      display: none;
+    } 
+
+    @media (min-width: 1280px){
+      width: 100%;
+      height: unset;
+
+      >span {
+        display: inline;
+      }
+    }
+  }
+`;
 
 const iconCSS = css`
   flex-shrink: 0;
